3506929bd59b921b38c2e1113b4b47d69e8b1131
Alex Purdy <apurdy@uvic.ca>
2017-03-19 20:47:54 -0700

    milestone 1 complete - data extraction

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,22 @@
 package observatory
 
+import java.io.InputStream
 import java.time.LocalDate
+
+import scala.io.Source
+
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  def toCelsius(fahrenheit: Double): Double = (fahrenheit - 32) * 5.0 / 9.0
+
+  def loader(test: Boolean): String => InputStream = {
+    if (test) getClass.getClassLoader.getResourceAsStream(_)
+    else getClass.getResourceAsStream(_)
+  }
 
   /**
     * @param year             Year number
@@ -13,16 +24,66 @@ object Extraction {
     * @param temperaturesFile Path of the temperatures resource file to use (e.g. "/1975.csv")
     * @return A sequence containing triplets (date, location, temperature)
     */
-  def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
-    ???
+  def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String, test: Boolean = false): Iterable[(LocalDate, Location, Double)] = {
+
+    // a little hack to load test resources for unit-testing
+    val loaderFunc = loader(test)
+
+    val stationMap = Source
+      .fromInputStream(loaderFunc(stationsFile))
+      .getLines()
+      .map(_.split(",", -1))
+      // drop records missing both station identifiers or geographic location
+      // format is (STN id, WBAN id, latitude, longitude)
+      .filter(array => (array(0).nonEmpty || array(1).nonEmpty) &&
+      !array.slice(2, 4).contains(""))
+      // convert to lookup map station id -> location
+      .map(array => (array(0), array(1)) -> Location(array(2).toDouble, array(3).toDouble))
+      .toMap
+
+    Source
+      .fromInputStream(loaderFunc(temperaturesFile))
+      .getLines()
+      .map(_.split(",", -1))
+      // drop records missing both station ids temperature or date data
+      // format is (STN id, WBAN id, month, day, temperature)
+      .filter(array =>
+      (array(0).nonEmpty || array(1).nonEmpty) &&
+        !array.slice(2, 5).contains("") &&
+        array(4).toDouble != 9999.0)
+      .map(array => {
+        val localDate = LocalDate.of(year, array(2).toInt, array(3).toInt)
+        // look up location data
+        val maybeLocation = stationMap.get((array(0), array(1)))
+        val temperature = toCelsius(array(4).toDouble)
+        (localDate, maybeLocation, temperature)
+      })
+      // drop any temperature records that were missing location data (no corresponding record in stations.csv)
+      .collect { case (localDate, Some(location), temperature) => (localDate, location, temperature) }
+      // sorting is required due to grader idiosyncrasies. remove if grader is fixed
+      .toList.sortBy(tpl => (tpl._1.getMonthValue, tpl._1.getDayOfMonth))
   }
 
   /**
     * @param records A sequence containing triplets (date, location, temperature)
     * @return A sequence containing, for each location, the average temperature over the year.
     */
-  def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Double)]): Iterable[(Location, Double)] = {
-    ???
-  }
-
+  def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Double)]): Iterable[(Location, Double)] = records
+    .groupBy(_._2)
+    .mapValues { iterable =>
+      val size = iterable.size
+      val sum = iterable.map(_._3).sum
+      sum / size
+    }.toStream
 }
+
+object ExtractTestData extends App {
+
+  import Extraction._
+
+  val results = locateTemperatures(2000, "/stations.csv", "/2000.csv")
+  results.take(10).foreach(println)
+  val averages = locationYearlyAverageRecords(results)
+  averages.take(10).foreach(println)
+}
+
--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -1,11 +1,27 @@
 package observatory
 
+import java.time.LocalDate
+
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
+import Extraction._
 
 @RunWith(classOf[JUnitRunner])
 class ExtractionTest extends FunSuite {
 
-  
+  val year = 2000
+  val results: Iterable[(LocalDate, Location, Double)] =
+    locateTemperatures(year, "test_stations.csv", "test_temperatures.csv", test = true)
+
+  test("weather stations are identified by the composite (STN, WBAN)") {
+    assert(results.size === 9)
+  }
+
+  test("average temperature by location") {
+    val averageTemps = locationYearlyAverageRecords(results)
+    val sorted = averageTemps.toList.sortBy(_._1.lat)
+    assert(sorted.head._1 === Location(1.0, -1.0))
+    assert(sorted.head._2 === toCelsius(22))
+  }
 }
