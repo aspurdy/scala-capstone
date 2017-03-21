6bb5c8362033ae5b2de132cd48832b3017030784
Alex Purdy <apurdy@uvic.ca>
2017-03-20 23:59:09 -0700

    first pass at week 2 - visualization

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import java.io.File
+
 import com.sksamuel.scrimage.{Image, Pixel}
 
 /**
@@ -7,32 +9,109 @@ import com.sksamuel.scrimage.{Image, Pixel}
   */
 object Visualization {
 
+  implicit class RichLocation(loc: Location) {
+
+    import math.{cos, abs, acos, sin}
+
+    val earthRadius = 6371
+
+    def greatCircleDistance(other: Location): Double = {
+      val lonDelta = abs(loc.lon - other.lon)
+      val centralAngle = acos(sin(loc.lat) * sin(other.lat) + cos(loc.lat) * cos(other.lat) * cos(lonDelta))
+      earthRadius * centralAngle
+    }
+  }
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+    temperatures.find(_._1 == location) match {
+      case Some((_, temperature)) => temperature
+      case None =>
+        // inverse distance weighting (basic form)
+        // see https://en.wikipedia.org/wiki/Inverse_distance_weighting
+        val (totalWeighted, totalWeights) = temperatures.foldLeft(0.0, 0.0) { (acc, point) =>
+          val weight = 1 / location.greatCircleDistance(point._1)
+          val weighted = weight * point._2
+          (acc._1 + weighted, acc._2 + weight)
+        }
+        totalWeighted / totalWeights
+    }
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    import math._
+
+    def clamp(x: Double) = 0.0.max(x).min(1.0)
+
+    def scale(xMin: Double, xMax: Double, x: Double) = (x - xMin) / (xMax - xMin)
+
+    def lerp(v0: Double, v1: Double, t: Double) = (1 - t) * v0 + t * v1
+
+    val pointsMap = points.toMap
+    val (t1, c1, t2, c2) = {
+      val first = pointsMap.minBy(point => abs(point._1 - value))
+      val second = pointsMap - first._1 minBy (point => abs(point._1 - value))
+      if (first._1 < second._1) (first._1, first._2, second._1, second._2)
+      else (second._1, second._2, first._1, first._2)
+    }
+
+    val t = clamp(scale(min(t1, t2), max(t1, t2), value))
+
+    val r = lerp(c1.red, c2.red, t)
+    val g = lerp(c1.green, c2.green, t)
+    val b = lerp(c1.blue, c2.blue, t)
+    Color(round(r).toInt, round(g).toInt, round(b).toInt)
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
-    ???
-  }
+    val locations = for {
+      y <- Range(0, 180)
+      x <- Range(0, 360)
+    } yield Location(x - 180, -y + 90)
 
+    val pixels = locations.par.map { location =>
+      val predictedTemp = predictTemperature(temperatures, location)
+      val color = interpolateColor(colors, predictedTemp)
+      Pixel(color.red, color.green, color.blue, 255)
+    }
+
+    Image(360, 180, pixels.toArray)
+  }
 }
 
+object VisApp extends App {
+
+  import Extraction._
+
+  val results = locateTemperatures(2000, "/stations.csv", "/2000.csv")
+  val averages = locationYearlyAverageRecords(results)
+
+  val colorScale = List(
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  )
+
+  val image = Visualization.visualize(temperatures = averages, colors = colorScale)
+
+  image.output(new File("target/test.png"))
+}
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -9,5 +9,66 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
 
+  import Visualization._
 
+  val white = Color(255, 255, 255)
+  val black = Color(0, 0, 0)
+
+  val colorScale = List(
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  )
+
+  test("degenerate case") {
+    val temperatures = List(
+      (Location(0, 0), 10.0),
+      (Location(10, -10), 0.0)
+    )
+
+    val result = predictTemperature(temperatures, Location(0, 0))
+    assert(result === 10.0)
+  }
+
+  test("midpoint between antipodal points should give average") {
+    val temperatures = List(
+      (Location(10, 10), 0.0),
+      (Location(-10, -10), 10.0)
+    )
+
+    val result = predictTemperature(temperatures, Location(0, 0))
+    assert(result === 5.0)
+  }
+
+  test("should interpolate colors") {
+    val points = List((0.0, white), (10.0, black))
+
+    val res = interpolateColor(points, 5.0)
+    assert(res === Color(128, 128, 128))
+  }
+
+  test("should interpolate colors - color order reversed") {
+    val points = List((0.0, black), (10.0, white))
+
+    val res = interpolateColor(points, 5.0)
+    assert(res === Color(128, 128, 128))
+  }
+
+  test("interpolate over max range ") {
+    val points = List((0.0, black), (10.0, white))
+
+    val res = interpolateColor(points, 11.0)
+    assert(res === white)
+  }
+  test("interpolate under min range ") {
+    val points = List((0.0, black), (10.0, white))
+
+    val res = interpolateColor(points, -1.0)
+    assert(res === black)
+  }
 }
