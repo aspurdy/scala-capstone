dd2d35171aedfd799f7f0b1fa9ae053f7aeffdda
Alex Purdy <apurdy@uvic.ca>
2017-03-31 16:56:44 -0700

    Milestone 5 - value-added information visualization complete

--- src/main/scala/observatory/Visualization2.scala
+++ src/main/scala/observatory/Visualization2.scala
@@ -1,15 +1,18 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import Interaction.tileLocation
+import Visualization.interpolateColor
 
 /**
   * 5th milestone: value-added information visualization
   */
 object Visualization2 {
+  val tileSize = 256
 
   /**
-    * @param x X coordinate between 0 and 1
-    * @param y Y coordinate between 0 and 1
+    * @param x   X coordinate between 0 and 1
+    * @param y   Y coordinate between 0 and 1
     * @param d00 Top-left value
     * @param d01 Bottom-left value
     * @param d10 Top-right value
@@ -18,32 +21,60 @@ object Visualization2 {
     *         See https://en.wikipedia.org/wiki/Bilinear_interpolation#Unit_Square
     */
   def bilinearInterpolation(
-    x: Double,
-    y: Double,
-    d00: Double,
-    d01: Double,
-    d10: Double,
-    d11: Double
-  ): Double = {
-    ???
+                             x: Double,
+                             y: Double,
+                             d00: Double,
+                             d01: Double,
+                             d10: Double,
+                             d11: Double
+                           ): Double = {
+    d00 * (1 - x) * (1 - y) + d10 * x * (1 - y) + d01 * (1 - x) * y + d11 * x * y
   }
 
   /**
-    * @param grid Grid to visualize
+    * @param grid   Grid to visualize
     * @param colors Color scale to use
-    * @param zoom Zoom level of the tile to visualize
-    * @param x X value of the tile to visualize
-    * @param y Y value of the tile to visualize
+    * @param zoom   Zoom level of the tile to visualize
+    * @param x      X value of the tile to visualize
+    * @param y      Y value of the tile to visualize
     * @return The image of the tile at (x, y, zoom) showing the grid using the given color scale
     */
   def visualizeGrid(
-    grid: (Int, Int) => Double,
-    colors: Iterable[(Double, Color)],
-    zoom: Int,
-    x: Int,
-    y: Int
-  ): Image = {
-    ???
+                     grid: (Int, Int) => Double,
+                     colors: Iterable[(Double, Color)],
+                     zoom: Int,
+                     x: Int,
+                     y: Int
+                   ): Image = {
+
+    def clampLongitude(lon: Int): Int = ((lon + 180) % 360) - 180
+
+    def clampLatitude(lat: Int): Int = ((lat - 90) % 180) + 90
+
+    val pixels = for {
+      yOffset <- (0 until tileSize).par
+      xOffset <- (0 until tileSize).par
+      location = tileLocation(zoom, x + (xOffset / tileSize.toDouble), y + (yOffset / tileSize.toDouble))
+      // find bounding grid coordinates
+      // boundary conditions: latitude in [-89, 90] and a longitude in [-180, 179]
+      x0 = clampLongitude(math.floor(location.lon).toInt)
+      x1 = clampLongitude(math.ceil(location.lon).toInt)
+      y0 = clampLatitude(math.ceil(location.lat).toInt)
+      y1 = clampLatitude(math.floor(location.lat).toInt)
+      // grid temperatures
+      t00 = grid(y0, x0)
+      t01 = grid(y1, x0)
+      t10 = grid(y0, x1)
+      t11 = grid(y1, x1)
+
+      // lat/lon offsets [0, 1] inside unit square
+      deltaX = location.lon - x0
+      deltaY = y0 - location.lat
+      t = bilinearInterpolation(deltaX, deltaY, t00, t01, t10, t11)
+      color = interpolateColor(colors, t)
+    } yield Pixel(color.red, color.green, color.blue, 127)
+
+    Image(tileSize, tileSize, pixels.toArray)
   }
 
 }
--- src/test/scala/observatory/Visualization2Test.scala
+++ src/test/scala/observatory/Visualization2Test.scala
@@ -1,12 +1,71 @@
 package observatory
 
+import java.io.File
+
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
+import Visualization2._
 
 @RunWith(classOf[JUnitRunner])
 class Visualization2Test extends FunSuite with Checkers {
+
+  val colorScale = List(
+    (60.0, Color(255, 255, 255)),
+    (-60.0, Color(0, 0, 0))
+  )
+
+  def euclideanDistance(c1: Color, c2: Color): Double = {
+    val rd = c2.red - c1.red
+    val gd = c2.green - c1.green
+    val bd = c2.blue - c1.blue
+    math.sqrt(rd * rd + gd * gd * bd * bd)
+  }
+
+  test("bilinearInterpolation") {
+    val result = bilinearInterpolation(0.5, 0.5, 5, 10, -5, -10)
+    assert(result == 0.0)
+  }
+
+  test("visualizeGrid - zoom 0") {
+    import Manipulation.makeGrid
+
+    val (temp1, color1) = colorScale.head
+    val (temp2, color2) = colorScale.last
+
+    val temperatures = List(
+      (Location(45, -50), temp1), // upper left
+      (Location(-45, 50), temp2) // lower right
+    )
+
+    val grid = makeGrid(temperatures)
+    val img = visualizeGrid(grid, colorScale, zoom = 0, x = 0, y = 0)
+    //    img.output(new File("/home/alex/IdeaProjects/scala-capstone/observatory/test-zoom-0.png"))
+    val pixel = img.pixel(25, 25)
+    val color = Color(pixel.red, pixel.green, pixel.blue)
+    assert(euclideanDistance(color, color1) < euclideanDistance(color, color2))
+  }
+
+  test("visualizeGrid - zoom 1") {
+    import Manipulation.makeGrid
+
+    val (temp1, color1) = colorScale.head
+    val (temp2, color2) = colorScale.last
+
+    val temperatures = List(
+      (Location(45, -50), temp1), // upper left
+      (Location(-45, 50), temp2) // lower right
+    )
+
+    val grid = makeGrid(temperatures)
+    // lower right quadrant
+    val img = visualizeGrid(grid, colorScale, zoom = 1, x = 1, y = 1)
+    //    img.output(new File("/home/alex/IdeaProjects/scala-capstone/observatory/test-zoom-1.png"))
+    val pixel = img.pixel(25, 25)
+    val color = Color(pixel.red, pixel.green, pixel.blue)
+    assert(euclideanDistance(color, color2) < euclideanDistance(color, color1))
+  }
 
 
 }
